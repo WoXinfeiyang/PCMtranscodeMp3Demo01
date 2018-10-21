963a8cee4068f56452cbecdb3bc08edd8fbef298
andyfu <815965325@qq.com>
2018-10-21 21:39:39 +0800

    1、修改pcmToMp3.h中方法签名；2、实现PCMtoMp3类(开发中…………)

---
+++ app/src/main/jni/pcm_to_mp3/pcmToMp3.cpp
@@ -0,0 +1,21 @@
+#include"pcmToMp3.h"
+PCMtoMp3::PCMtoMp3(){
+}
+
+PCMtoMp3::~PCMtoMp3(){
+}
+
+PCMtoMp3::int Init(int audioChannels, int bitRate, int sampleRate){
+    int result=-1;
+
+    return result;
+}
+
+
+PCMtoMp3::void PCMtranscodeMp3(const char* pcmFilePath,const char* mp3FilePath){
+
+}
+
+PCMtoMp3::void Destory(){
+
+}
